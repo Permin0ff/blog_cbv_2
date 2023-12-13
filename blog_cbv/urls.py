9181768f36cfe7fe936f85e2702a1c0a2093068e
Ilya <perminoff-ilya@yandex.ru>
2023-12-13 09:42:01 +0300

    Course 11.7

--- blog_cbv/urls.py
+++ blog_cbv/urls.py
@@ -18,18 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from apps.blog.feeds import LatestPostFeed
 
 urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
-
     path('admin/', admin.site.urls),
+    path('feeds/latest/', LatestPostFeed(), name='latest_post_feed'),
     path('', include('apps.blog.urls')),
     path('', include('apps.accounts.urls')),
-
 ]
-
-
 if settings.DEBUG:
-
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += [path('__debug__/', include('debug_toolbar.urls'))]
