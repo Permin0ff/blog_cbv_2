07aca40a3c72fc6edb36228c36dc4b2b05bc83d7
Ilya <perminoff-ilya@yandex.ru>
2023-12-15 08:18:56 +0300

    Course 11.10

--- blog_cbv/settings.py
+++ blog_cbv/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p%d2_o0fe=l1v3%k_e&b=%v631c_4hjjt9!f6=8avx!r4++%k$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
